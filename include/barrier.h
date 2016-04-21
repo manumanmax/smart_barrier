8cf4b3705f5c4e385d9a76514713930fef96807c
manumanmax <manua380@hotmail.fr>
2016-04-21 22:17:13 +0000

    adding structure and enum on barrier

--- include/barrier.h
+++ include/barrier.h
@@ -1,6 +1,18 @@
 #ifndef BARRIER_H
 #define BARRIER_H
 
-void raise();
+typedef enum Position Position;
+enum Position
+{
+	UP,DOWN
+};
+
+typedef Struct Barrier{
+	int startAngle;
+	Position pos;	
+} Barrier;
+
+void raiseBarrier();
+void setupBarrier();
 
 #endif
--- src/barrier.c
+++ src/barrier.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <wiringPi.h>
 
-void raise(){
+void raiseBarrier(){
 	printf("raising the barrier\n");
 }
 
